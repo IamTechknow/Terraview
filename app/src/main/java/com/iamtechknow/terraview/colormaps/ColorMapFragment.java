83b4b2c1109ac7c2f194a96bf5e23e49b0ba5111
Edmond Lee <techhexium@gmail.com>
2018-04-15 10:58:46 -0700

    Get rid of Restricted API for bottom sheet fragment

--- app/src/main/java/com/iamtechknow/terraview/colormaps/ColorMapFragment.java
+++ app/src/main/java/com/iamtechknow/terraview/colormaps/ColorMapFragment.java
@@ -1,60 +1,34 @@
 package com.iamtechknow.terraview.colormaps;
 
-import android.app.Dialog;
+import android.os.Bundle;
 import android.support.annotation.NonNull;
-import android.support.design.widget.BottomSheetBehavior;
 import android.support.design.widget.BottomSheetDialogFragment;
-import android.support.design.widget.CoordinatorLayout;
 import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 
 import com.iamtechknow.terraview.R;
 import com.iamtechknow.terraview.adapter.ColorMapAdapter;
-import com.iamtechknow.terraview.model.Layer;
-
-import java.util.ArrayList;
 
 public class ColorMapFragment extends BottomSheetDialogFragment {
     public static final String COLORMAP_ARG = "layers";
 
     /**
-     * Behaviour for the bottom sheet, just dismiss it when swiped down
-     */
-    private BottomSheetBehavior.BottomSheetCallback mCallback = new BottomSheetBehavior.BottomSheetCallback() {
-        @Override
-        public void onStateChanged(@NonNull View bottomSheet, int newState) {
-            if(newState == BottomSheetBehavior.STATE_HIDDEN)
-                dismiss();
-        }
-
-        @Override
-        public void onSlide(@NonNull View bottomSheet, float slideOffset) {}
-    };
-
-    /**
      * Setup the bottom sheet behaviour and pass the current layer data to the list
      */
     @Override
-    public void setupDialog(Dialog dialog, int style) {
-        super.setupDialog(dialog, style);
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.color_map_frag, container);
 
-        View contentView = View.inflate(getContext(), R.layout.color_map_frag, null);
-        dialog.setContentView(contentView);
-
-        CoordinatorLayout.LayoutParams layoutParams =
-                (CoordinatorLayout.LayoutParams) ((View) contentView.getParent()).getLayoutParams();
-        CoordinatorLayout.Behavior behavior = layoutParams.getBehavior();
-        if (behavior != null && behavior instanceof BottomSheetBehavior) {
-            ((BottomSheetBehavior) behavior).setBottomSheetCallback(mCallback);
-            ((BottomSheetBehavior) behavior).setState(BottomSheetBehavior.STATE_EXPANDED);
-        }
-        ArrayList<Layer> colorMapLayers = getArguments().getParcelableArrayList(COLORMAP_ARG);
-
-        RecyclerView mRecyclerView = (RecyclerView) contentView.findViewById(R.id.color_map_rv);
+        RecyclerView mRecyclerView = (RecyclerView) rootView.findViewById(R.id.color_map_rv);
         mRecyclerView.setItemAnimator(new DefaultItemAnimator());
         mRecyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
-        mRecyclerView.setAdapter(new ColorMapAdapter(colorMapLayers));
+        if(getArguments() != null)
+            mRecyclerView.setAdapter(new ColorMapAdapter(getArguments().getParcelableArrayList(COLORMAP_ARG)));
+
+        return rootView;
     }
 }
